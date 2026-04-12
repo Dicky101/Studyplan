9a52991faf12e75842130b15f4bf13df1b348eab
Cheung Wang Hei <dickycheung2005@gmail.com>
2026-04-12 15:33:49 +0800

    change

--- getcatolog.py
+++ getcatolog.py
@@ -1,11 +1,21 @@
-import requests
 import re
 from io import BytesIO
-from pypdf import PdfReader
 
 import requests
-import re
 from bs4 import BeautifulSoup
+from pdfminer.high_level import extract_text
+from pypdf import PdfReader
+
+def extract_text_from_pdf(pdf_url: str) -> str:
+    response = requests.get(pdf_url, timeout=30)
+    response.raise_for_status()
+
+    with BytesIO(response.content) as pdf_file:
+        text = extract_text(pdf_file)
+
+    return text
+  # Clean readable text
+
 
 def get_info_from_website(url, *filters) -> list[str]:
     response = requests.get(url, timeout=30)
@@ -40,4 +50,8 @@ def extract_math_courses_from_pdf_url(pdf_url: str) -> list[str]:
     course_codes = re.findall(r'\b(MATH\s+\d{4})\b', all_text, re.IGNORECASE)
     return sorted(set(course_codes))  # unique, sorted
 
-# Example
+
+if __name__ == "__main__":
+    pdf_url = "https://ugadmin.hkust.edu.hk/prog_crs/ug/202526/pdf/25-26math.pdf"
+    math_courses = extract_text_from_pdf(pdf_url)
+    print("MATH courses found:", math_courses[2000:2500])  # Print the first 500 characters of the extracted text
--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from Myclass import Myclass
 
 #input from user
 # year=input("Enter the year of admission: ")
-# school=input("Enter the school: ")
-# major=input("Enter the semester: ")
-# max_credits=int(input("Enter the maximum credits you can take: "))
+year = "2023-24"
 
+school = ["SENG","SBM","SSCI","SHSS","AIS"]
 
 #get course codes
 course_code_list=get_info_from_website("https://prog-crs.hkust.edu.hk/ugcourse", r'[A-Z]{4}')
 print("Course codes:", course_code_list)
 
 #get the course list of the major
-year = "2024-25"
 major_list = get_info_from_website("https://prog-crs.hkust.edu.hk/ugprog/" + year,  r'[A-Z]{2,6}',r'[A-Z]+-[A-Z]+')
 print("Major list:", major_list)
+
+# school=input("Enter the school: ")
+# major=input("Enter the semester: ")
+# max_credits=int(input("Enter the maximum credits you can take: "))
+major="ELEC"
+
+course=get_info_from_website("https://ugadmin.hkust.edu.hk/prog_crs/ug/"+year.replace('-', '')+"/pdf/"+year[2:]+major.lower()+".pdf",r'.*')
+print("Course list of the major:", course)
